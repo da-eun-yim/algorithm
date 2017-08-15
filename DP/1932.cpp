045915d2fca92e8344c31e50ddbd27469a7a04f7
da-eun-yim <yimdaeun94@gmail.com>
2017-08-15 21:22:50 +0900

    DP baekjoon solving

---
+++ DP/1932.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1932.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/15/2017 19:23:45
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int sum(int toFind){
+
+}
+
+int main(){
+	int totalRow
+	vector<int> Rows;
+
+}
